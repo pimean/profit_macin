d59c545f74743afb3516701b18fbf4b0a7abef98
pimean <pimeanrem@gmail.com>
2023-04-07 19:01:55 -0700

    Save Logo

---
+++ schema.py
@@ -0,0 +1,21 @@
+schema_markup_business_tools_profit_margin_calculator = {
+    "@context": "https://schema.org",
+    "@type": "SoftwareApplication",
+    "name": "Profit Margin Calculator",
+    "description": "A free online tool for calculate profit margin for business.",
+    "applicationCategory": "Business Tool",
+    "operatingSystem": "Web-based",
+    "browserRequirements": "Web browser with JavaScript support",
+    "image": "https://example.com/images/tool1.png",
+    "brand": {
+        "@type": "Brand",
+        "name": "Your brand name"
+    },
+
+    "offers": {
+        "@type": "Offer",
+        "price": "0",
+        "url": "https://www.profitmacin.com/profit-margin-calculator"
+    }
+
+}
