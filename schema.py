8fe39f96d6aa84229cf630dfb85ef29744161fbd
pimean <pimeanrem@gmail.com>
2023-04-07 21:08:15 -0700

    Adding Schema

--- schema.py
+++ schema.py
@@ -7,28 +7,30 @@ schema_markup_business_tools_profit_margin_calculator = {
     "applicationCategory": "BusinessApplication",
     "operatingSystem": "Web-based",
     "browserRequirements": "Web browser with JavaScript support",
-    "image": "https://example.com/images/tool1.png",
+    "url": "https://www.profitmacin.com/profit-margin-calculator",
+    "keywords": "profit margin calculator, free financial tool, business calculator",
+    "isAccessibleForFree": True,
 
     "offers": {
         "@type": "Offer",
         "price": "0",
-        "url": "https://www.profitmacin.com/profit-margin-calculator"
     },
 
     "image": [
-        {
-            "@type": "ImageObject",
-            "url": "https://www.profitmacin.com/static/images/schema/profit-margin-calculator-app-screenshot.jpg",
-            "width": "1200",
-            "height": "628"
-        },
         {
             "@type": "ImageObject",
             "url": "https://www.profitmacin.com/static/images/schema/profit-margin-calculator-app-logo.png",
             "width": "180",
             "height": "180"
         }
-    ],
+    ],    
+
+    "screenshot": {
+        "@type": "ImageObject",
+        "url": "https://www.profitmacin.com/static/images/schema/profit-margin-calculator-app-screenshot.jpg",
+        "width": "1200",
+        "height": "628"
+    },
 
     "provider": {
         "@type": "Organization",
@@ -36,11 +38,63 @@ schema_markup_business_tools_profit_margin_calculator = {
         "logo": {
             "@type": "ImageObject",
             "url": "https://www.profitmacin.com/static/images/profitmacin-official-logo.png",
-            "width": "600",
-            "height": "600"
+            "width": "500",
+            "height": "500"
         }
     },
 
+    "publisher": {
+        "@type": "Organization",
+        "name": "Profit Macin",
+        "logo": {
+            "@type": "ImageObject",
+            "url": "https://www.profitmacin.com/static/images/profitmacin-official-logo.png",
+            "width": "500",
+            "height": "500"
+        }
+    },
 
+    "creator": {
+        "@type": "Person",
+        "name": "Bong Mean",
+        "jobTitle": "Web Developer",
+        "url": "https://www.kongrei.com/"
+    },
+    "creditText": "Developed by Kongrei Team.",
+
+    "breadcrumb": {
+        "@type": "BreadcrumbList",
+        "itemListElement": [
+            {
+                "@type": "ListItem",
+                "position": 1,
+                "name": "Home",
+                "item": "https://www.profitmacin.com"
+            },
+            {
+                "@type": "ListItem",
+                "position": 2,
+                "name": "Business Tools",
+                "item": "https://www.profitmacin.com/business-tools"
+            },
+            {
+                "@type": "ListItem",
+                "position": 3,
+                "name": "Profit Margin Calculator",
+                "item": "https://www.profitmacin.com/profit-margin-calculator"
+            }
+        ]
+    },
+
+    "copyrightHolder": {
+        "@type": "Organization",
+        "name": "Profit Macin"
+    },
+    "copyrightNotice": "Copyright © 2023 Profit Macin. All rights reserved.",
+    "copyrightYear": "2023",
+
+    "dateCreated": "2023-04-07",
+    "dateModified": "2023-04-07",
+    "datePublished": "2023-04-07"
 
 }
