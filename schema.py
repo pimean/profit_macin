8559ef393757f4cd03a5402eb9d1ba8bd7b41e97
pimean <pimeanrem@gmail.com>
2023-04-07 19:24:08 -0700

    edit logo

--- schema.py
+++ schema.py
@@ -7,15 +7,24 @@ schema_markup_business_tools_profit_margin_calculator = {
     "operatingSystem": "Web-based",
     "browserRequirements": "Web browser with JavaScript support",
     "image": "https://example.com/images/tool1.png",
-    "brand": {
-        "@type": "Brand",
-        "name": "Your brand name"
-    },
 
     "offers": {
         "@type": "Offer",
         "price": "0",
         "url": "https://www.profitmacin.com/profit-margin-calculator"
-    }
+    },
+
+    "provider": {
+        "@type": "Organization",
+        "name": "Profit Macin",
+        "logo": {
+            "@type": "ImageObject",
+            "url": "https://www.profitmacin.com/static/images/profitmacin-official-logo.png",
+            "width": "500",
+            "height": "500"
+        }
+    },
+
+
 
 }
